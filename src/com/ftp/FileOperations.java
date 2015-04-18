2e872a595cb756164d242af162958695c38c2091
aneeshkher <aneeshkher@gmail.com>
2015-04-18 18:17:33 -0400

    Add test code for getting contents of file and converting to bytes

--- src/com/ftp/FileOperations.java
+++ src/com/ftp/FileOperations.java
@@ -2,6 +2,7 @@ package com.ftp;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,13 +11,30 @@ public class FileOperations {
 
 	public void openAndReadFile (String fileName) throws IOException {
 		ArrayList<String> lines = new ArrayList<>();
-		
+		ArrayList<Byte> byteLines = new ArrayList<>();
 		String line;
 		File file = new File(fileName);
+		Long fLength;
+		fLength = file.length();
+		System.out.println("Length of file: " + file.length());
+		FileInputStream f1 = new FileInputStream(file);
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(file));
+			BufferedReader copy = new BufferedReader(new FileReader(file));
+			int fileLength = getFileLength(copy);
+			copy.close();
+			byte[] bytes = new byte[fLength.intValue()];
+			System.out.println("Length of file: " + fileLength );
 			while ((line = reader.readLine()) != null) {
+				//byte[] temp = line.getBytes();
 				lines.add(line);
+			}
+			f1.read(bytes);
+			for (byte b : bytes) {
+				//System.out.println("Reading byte");
+				//System.out.println(b);
+				String s = new String(new byte[]{b});
+				System.out.print(s);
 			}
 			reader.close();
 		} catch (IOException e1) {
@@ -30,9 +48,23 @@ public class FileOperations {
 		}
 		
 		for (String companies : lines) {
-			System.out.println(companies);
+			//System.out.println(companies);
+		}
+	}
+	
+	private static int getFileLength (BufferedReader reader) {
+		String line;
+		int count = 0;
+		try {
+			while ((line = reader.readLine()) != null) {
+				count = count + line.length();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			
 		}
 		
-	
+		return count;
 	}
 }
--- src/com/ftp/SimpleFTPServer.java
+++ src/com/ftp/SimpleFTPServer.java
@@ -1,6 +1,8 @@
 package com.ftp;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.UnsupportedCharsetException;
 
 public class SimpleFTPServer {
 
@@ -10,7 +12,36 @@ public class SimpleFTPServer {
 		FileOperations f1 = new FileOperations();
 		
 		f1.openAndReadFile("AppliedCompanies.txt");
+		//testBytes();
 
+	}
+	public static void testBytes() {
+		/*String testString = "My Name is Aneesh Kher."
+				+ " Here are some weird chars ------- *&%&%"
+				+ "$(()!)*#^&$#";*/
+		String testString = "abcdefghijklmnopqrstuvwxyz"
+				+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+				+ "0123456789"
+				+ ")!@#$%^&*(_+=-;:|,\"<>.?/";
+		System.out.println("Original String:");
+		System.out.println(testString);
+		System.out.println("String Length: " + testString.length());
+		try {
+			byte[] bytes = testString.getBytes();
+			//System.out.println(bytes);
+			System.out.println("Length is: " + bytes.length);
+			System.out.println("Decoding -> ");
+			for (byte b : bytes) {
+				//System.out.println("First byte: " + b);
+				String s = new String(new byte[]{b});
+				System.out.println(b + " -> " + s);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			//System.out.println("Worked");
+		}
+		
 	}
 
 }
