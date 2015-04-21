dbf66335ac9da0454092abaeec248fa9d89a54d2
aneeshkher <aneeshkher@gmail.com>
2015-04-21 18:59:51 -0400

    Small additions

--- src/com/ftp/ReceiveRunnable.java
+++ src/com/ftp/ReceiveRunnable.java
@@ -6,15 +6,37 @@ import java.net.DatagramSocket;
 
 public class ReceiveRunnable implements Runnable {
 	private volatile boolean running = true;
-	private DatagramSocket udpClientSocket;
-	ReceiveRunnable(DatagramSocket udpSock)
-	{
-		udpClientSocket = udpSock;
+	public DatagramSocket udpClientSocket;
+
+	public ReceiveRunnable(DatagramSocket udpClientSocket) {
+		this.udpClientSocket = udpClientSocket;
 	}
+
 	public void terminate() {
 		running = false;
 	}
+
 	public void run() {
+		byte[] receivedBytes = new byte[8192];
+		DatagramPacket receivedPacket = new DatagramPacket(receivedBytes,
+				receivedBytes.length);
+		TestSimpleFTPClientData data = new TestSimpleFTPClientData();
+		while (running) {
+			while (true) {
+				try {
+					udpClientSocket.receive(receivedPacket);
+					synchronized (data) {
+						String received = new String(receivedPacket.getData());
+					}
+				} catch (IOException e1) {
+					
+				}
+				
+			} // End of while(true)
+		} // End of while(running)
+	} // End of run()
+
+	public void ru1n() {
 		System.out.println("Inside thread for receiving");
 		byte[] receivedBytes = new byte[8192];
 		DatagramPacket receivedPacket = new DatagramPacket(receivedBytes,
@@ -26,16 +48,13 @@ public class ReceiveRunnable implements Runnable {
 
 					udpClientSocket.receive(receivedPacket);
 					synchronized (this) {
-						String receive = new String(
-								receivedPacket.getData());
+						String receive = new String(receivedPacket.getData());
 						for (int i = 32; i < 64; i++) {
-							ACKNumberString = ACKNumberString
-									.concat(Character.toString(receive
-											.charAt(i)));
+							ACKNumberString = ACKNumberString.concat(Character
+									.toString(receive.charAt(i)));
 						}
 						receivedACK.add(ACKNumberString);
-						int seqNo = Integer.parseInt(ACKNumberString, 2)
-								- MSS;
+						int seqNo = Integer.parseInt(ACKNumberString, 2) - MSS;
 						unackData.remove(seqNo);
 						ACKed++;
 						System.out.println("Removed " + seqNo
